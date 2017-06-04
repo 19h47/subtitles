800ce2e04824a71f54594c238c876d374e7d774c
Levron Jérémy <levronjeremy@19h47.fr>
2017-06-04 15:00:43 +0200

    Subtitles are working 👊

--- src/js/main.js
+++ src/js/main.js
@@ -3,35 +3,47 @@ var parser = require('subtitles-parser');
 
 
 var subtitles = fs.readFileSync(__dirname + '/../subtitles/lost/s05/e05-this-place-is-death.srt', 'utf-8');
-
 var data = parser.fromSrt(subtitles, true);
-console.log(data);
-
-Object.keys(data).map(function(objectKey, index) {
-    var value = data[objectKey];
-
-    document.querySelector('#subtitles').innerHTML += value.text + '<br>';
-    console.log(value);
-});
-
 
 
 var start = null;
+var i = 0;
+
+// Set current var to avoid unnecessary DOM manipulation
+var current = false;
+var subtitlesEl = document.querySelector('#subtitles')
 
 function step(timestamp) {
-  	var progress;
-  	
-  	if (start === null) {
-  		start = timestamp;
-  	}
+	var progress;
+	
+	if (start === null) {
+		start = timestamp;
+	}
 
-  	progress = timestamp - start;
+	progress = timestamp - start;
 
-  	// console.log(progress);
-  	
-  	
-    requestAnimationFrame(step);
-  	
+	// StartTime
+	if (data[i].startTime <= progress && !current) {
+
+		current = true;
+
+		subtitlesEl.innerHTML = data[i].text + '<br>';
+	
+	}
+
+	// EndTime
+	if (data[i].endTime > data[i].startTime && data[i].endTime <= progress) {
+
+		subtitlesEl.innerHTML = '';
+
+		// Current
+		current = false;
+		// Pass to next entry
+		i++;
+	}
+
+	requestAnimationFrame(step);
+		
 }
 
 requestAnimationFrame(step);
